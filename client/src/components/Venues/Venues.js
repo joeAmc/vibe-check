93e64e9289dc99eb79463c17d782103ac1030eb9
joeAmc <joe@limio.com>
2023-05-29 23:13:39 +0100

    no vibes placeholder

--- client/src/components/Venues/Venues.js
+++ client/src/components/Venues/Venues.js
@@ -1,5 +1,4 @@
-import React from "react";
-import { useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import "./Venues.css";
 import NewVIbeButton from "../NewVIbeButton/NewVIbeButton";
@@ -7,9 +6,10 @@ import Nav from "../Nav/Nav";
 
 const Venues = () => {
   const { type } = useParams();
-  const [venues, setVenues] = useState([]);
+  const [venues, setVenues] = useState(null);
   const [veriVibed, setVeriVibed] = useState(false);
   const [selectedVenue, setSelectedVenue] = useState(null);
+  const [hasVenuesOfType, setHasVenuesOfType] = useState(false); // New state variable
 
   const removeUnderScoreFromType = type.replaceAll("_", " ");
 
@@ -20,7 +20,13 @@ const Venues = () => {
   const getVenues = () => {
     fetch("http://localhost:3002/venues")
       .then((res) => res.json())
-      .then((data) => setVenues(data))
+      .then((data) => {
+        const filteredVenues = data.filter(
+          (venue) => venue.type === type && venue.vibes >= 0
+        );
+        setVenues(filteredVenues);
+        setHasVenuesOfType(filteredVenues.length > 0);
+      })
       .catch((err) => console.log("Error: ", err));
   };
 
@@ -66,38 +72,47 @@ const Venues = () => {
       .catch((err) => console.log("Error: ", err));
   };
 
+  console.log("venues", venues);
+
   return (
     <div>
       <Nav venuesType={type} formatedType={removeUnderScoreFromType} />
       <div className="spacer" />
       <div className="venues">
-        {venues
-          .filter((venue) => venue.type === type && venue.vibes >= 0)
-          .reverse()
-          .map((venue) => (
-            <div className="venue-container" key={venue._id}>
-              <div className="checkbox"></div>
-              <div className="venue-img">
-                <img src={venue.image} alt="pub" />
-              </div>
-              <div className="name">
-                <h2>{venue.name}</h2>
-                <h4>{venue.location}</h4>
-              </div>
-              <div className="vibe-details">
-                <h5>Checked in - {formatDate(+venue.timestamp)}</h5>
-                <div className="vibe-votes">
-                  <div
-                    onClick={() => handleVibeClicked(venue)}
-                    className={`vibe-icon ${
-                      veriVibed && selectedVenue._id === venue._id && "selected"
-                    }`}
-                  ></div>
-                  <h5>{venue.vibes}</h5>
+        {hasVenuesOfType && venues.length > 0 ? (
+          venues
+            .filter((venue) => venue.type === type && venue.vibes >= 0)
+            .reverse()
+            .map((venue) => (
+              <div className="venue-container" key={venue._id}>
+                <div className="checkbox"></div>
+                <div className="venue-img">
+                  <img src={venue.image} alt="pub" />
+                </div>
+                <div className="name">
+                  <h2>{venue.name}</h2>
+                  <h4>{venue.location}</h4>
+                </div>
+                <div className="vibe-details">
+                  <h5>Checked in - {formatDate(+venue.timestamp)}</h5>
+                  <div className="vibe-votes">
+                    <div
+                      onClick={() => handleVibeClicked(venue)}
+                      className={`vibe-icon ${
+                        veriVibed &&
+                        selectedVenue &&
+                        selectedVenue._id === venue._id &&
+                        "selected"
+                      }`}
+                    ></div>
+                    <h5>{venue.vibes}</h5>
+                  </div>
                 </div>
               </div>
-            </div>
-          ))}
+            ))
+        ) : (
+          <h1>No current vibes</h1>
+        )}
       </div>
       <NewVIbeButton type={type} />
     </div>
