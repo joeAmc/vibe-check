f97c0068f5e32993169b6f5c969eb1019cc246e4
joeAmc <joe@limio.com>
2023-07-06 20:57:41 +0100

    env variables

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyC91rffnoM1s0mhbt29jS1x_zCaIpyhURk",
-  authDomain: "vibe-check-773b3.firebaseapp.com",
-  projectId: "vibe-check-773b3",
-  storageBucket: "vibe-check-773b3.appspot.com",
-  messagingSenderId: "186071599878",
-  appId: "1:186071599878:web:0760c05705a91671fd2c87",
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
